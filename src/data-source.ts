2147a321f06265ab6bc3961930eac426fef355c3
Breno Gabriel <bbbgab123@gmail.com>
2024-11-14 11:11:44 -0300

    modified:   node_modules/.yarn-integrity 	new file:   node_modules/@babel/plugin-proposal-class-properties/LICENSE 	new file:   node_modules/@babel/plugin-proposal-class-properties/README.md 	new file:   node_modules/@babel/plugin-proposal-class-properties/lib/index.js 	new file:   node_modules/@babel/plugin-proposal-class-properties/package.json 	new file:   node_modules/@babel/plugin-proposal-decorators/LICENSE 	new file:   node_modules/@babel/plugin-proposal-decorators/README.md 	new file:   node_modules/@babel/plugin-proposal-decorators/lib/index.js 	new file:   node_modules/@babel/plugin-proposal-decorators/lib/index.js.map 	new file:   node_modules/@babel/plugin-proposal-decorators/lib/transformer-legacy.js 	new file:   node_modules/@babel/plugin-proposal-decorators/lib/transformer-legacy.js.map 	new file:   node_modules/@babel/plugin-proposal-decorators/package.json 	new file:   node_modules/@babel/plugin-syntax-decorators/LICENSE 	new file:   node_modules/@babel/plugin-syntax-decorators/README.md 	new file:   node_modules/@babel/plugin-syntax-decorators/lib/index.js 	new file:   node_modules/@babel/plugin-syntax-decorators/lib/index.js.map 	new file:   node_modules/@babel/plugin-syntax-decorators/package.json 	modified:   package.json 	modified:   src/data-source.ts 	modified:   yarn.lock

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,8 +3,14 @@ import { DataSource } from "typeorm";
 export const datasource = new DataSource({
   type: "sqlite",
   database: "./database.sqlite",
-  entities: ["../entities/*/*.ts"],
-  migrations: ["../migrations/*.ts"],
+  entities:
+    process.env.NODE_ENV === "production"
+      ? [__dirname + "/../entities/*/*.js"]
+      : [__dirname + "/../entities/*/*.ts"],
+  migrations:
+    process.env.NODE_ENV === "production"
+      ? [__dirname + "/../migrations/*.js"]
+      : [__dirname + "/../migrations/*.ts"],
   synchronize: false,
   logging: true,
 });
