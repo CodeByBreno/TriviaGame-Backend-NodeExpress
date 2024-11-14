543bc755e6a34b9fcf23b95b466f06d03840fe9d
Breno Gabriel <bbbgab123@gmail.com>
2024-11-14 11:38:27 -0300

    Fix: Another try

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,17 @@
 import { DataSource } from "typeorm";
+import * as path from "path";
 
 export const datasource = new DataSource({
   type: "sqlite",
   database: "./database.sqlite",
   entities:
     process.env.NODE_ENV === "production"
-      ? [__dirname + "/entities/**/*.js"]
-      : [__dirname + "/entities/**/*.ts"],
+      ? [path.resolve(__dirname, "entities/**/*.js")]
+      : [path.resolve(__dirname, "entities/**/*.ts")],
   migrations:
     process.env.NODE_ENV === "production"
-      ? [__dirname + "/migrations/**/*.js"]
-      : [__dirname + "/migrations/**/*.ts"],
+      ? [path.resolve(__dirname, "migrations/**/*.js")]
+      : [path.resolve(__dirname, "migrations/**/*.ts")],
   synchronize: false,
   logging: true,
 });
